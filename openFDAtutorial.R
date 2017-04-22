90ca5dbe71174e020000db2665f7fb58f69594a8
Seraya Maouche <seraya.maouche@iscb.org>
2017-04-22 12:23:58 +0200

    Update and rename ROpenFDA.R to openFDAtutorial.R

---
+++ openFDAtutorial.R
@@ -0,0 +1,10 @@
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Script File: openFDAtutorial.R
+# Date of creation: 22 Apr 2017
+# Date of last modification: 22 Apr 2017
+# Author: Seraya Maouche <seraya.maouche@iscb.org>
+# Short Description: This script provides functions to access OpenFDA API from R
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# OpenFDA has not yet been added to CRAN
