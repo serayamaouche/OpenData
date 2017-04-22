74ea5179b8f620291265c25f8a260147c9d5d530
Seraya Maouche <seraya.maouche@iscb.org>
2017-04-22 13:18:21 +0200

    Update Eurostat2R.R

--- Eurostat2R.R
+++ Eurostat2R.R
@@ -12,9 +12,6 @@
 # Install Eurostat R Package
 install.packages("eurostat")
 
-# Load openfda
-library("eurostat")
-
 # Installing a dvelopment version from github
 library(devtools)
 install_github("ropengov/eurostat")
@@ -22,3 +19,69 @@ install_github("ropengov/eurostat")
 
 # Obtaining help
 # The Eurostat R package is based on the SmarterPoland package (https://cran.r-project.org/web/packages/SmarterPoland/index.html).
+
+# Load eurostat
+library(eurostat)
+library(rvest)
+library(xml2)
+
+# Get Eurostat data listing
+datasetList <- get_eurostat_toc()
+
+# # Check the first items
+library(knitr)
+kable(head(datasetList))
+|title                                                    |code      |type    |last update of data |last table structure change |data start |data end |values |
+|:--------------------------------------------------------|:---------|:-------|:-------------------|:---------------------------|:----------|:--------|:------|
+|Database by themes                                       |data      |folder  |NA                  |NA                          |NA         |NA       |NA     |
+|General and regional statistics                          |general   |folder  |NA                  |NA                          |NA         |NA       |NA     |
+|European and national indicators for short-term analysis |euroind   |folder  |NA                  |NA                          |NA         |NA       |NA     |
+|Business and consumer surveys (source: DG ECFIN)         |ei_bcs    |folder  |NA                  |NA                          |NA         |NA       |NA     |
+|Consumer surveys (source: DG ECFIN)                      |ei_bcs_cs |folder  |NA                  |NA                          |NA         |NA       |NA     |
+|Consumers - monthly data                                 |ei_bsco_m |dataset |30.03.2017          |30.03.2017                  |1980M01    |2017M03  |NA     |
+
+  
+# Search Eurostat for Cancer datasets  
+kable(head(search_eurostat("cancer")))
+|title                                                                                                                   |code           |type    |last update of data |last table structure change |data start |data end |values |
+|:-----------------------------------------------------------------------------------------------------------------------|:--------------|:-------|:-------------------|:---------------------------|:----------|:--------|:------|
+|Self-reported last colorectal cancer screening test by sex, age and educational attainment level                        |hlth_ehis_pa5e |dataset |20.03.2017          |20.03.2017                  |2014       |2014     |NA     |
+|Self-reported last colorectal cancer screening test by sex, age and degree of urbanisation                              |hlth_ehis_pa5u |dataset |21.02.2017          |NA                          |2014       |2014     |NA     |
+|Breast cancer and cervical cancer screenings                                                                            |hlth_ps_scre   |dataset |12.01.2017          |07.07.2016                  |2000       |2015     |NA     |
+|Self-reported colorectal cancer screening test by sex, age and educational attainment level among people aged 50-74 (%) |hlth_ehis_hc4  |dataset |20.03.2014          |29.08.2013                  |2008       |2008     |NA     |
+
+  
+# Downloading data from Eurostat
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
